23286b02fb11f593e7ccccf32cd875b6b4d8a0a9
CaseyFroehlich <caseyfroehlich8559@gmail.com>
2023-01-20 18:30:59 -0500

    Create Boot/Rest project, add JPA dependency

---
+++ MyBuilds/src/main/java/com/skilldistillery/projects/MyBuildsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBuildsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBuildsApplication.class, args);
+	}
+
+}
